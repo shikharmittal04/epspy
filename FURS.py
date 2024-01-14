158e2de794faaf05c535be0568c918741ae82358
mittal <shikhar.mittal4@gmail.com>
2024-01-14 12:17:04 +0530

    Added some messages

--- FURS.py
+++ FURS.py
@@ -87,13 +87,16 @@ if os.path.isfile(Tb_o_save_name) and os.path.isfile(beta_save_name):
 		
 		np.save(Tb_nu_save_name,Tb_nu_final)
 		print('Done.\n File saved as',Tb_nu_save_name)
+
+		#Since it's only one frequency, I am making a plot as well.		
 		plt.rc('text', usetex=True)
 		plt.rc('font', family='serif')
-		hp.mollview(Tb_nu_final,title=None,unit=r'$T_{\mathrm{b}}(\nu)\,$(K)',cmap=colormaps['coolwarm'],min=0.1,max=300,norm='log')
+		hp.mollview(Tb_nu_final,title=None,unit=r'$T_{\mathrm{b}}(\nu)\,$(K)',cmap=colormaps['coolwarm'],norm='log')
 		hp.graticule()
 		fig_path = path+'Tb_nu.pdf'
 		plt.savefig(fig_path, bbox_inches='tight')
 		print('\nTb map saved as',fig_path)
+
 	else:
 		print('Now computing the Tb at multiple frequencies ...')
 		Tb_nu_final = np.zeros((N_nu,Npix))	
