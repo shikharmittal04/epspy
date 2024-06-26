0b1964867812e2f35bb0ce0ee53be054cb45b977
Shikhar Mittal <60473059+shikharmittal04@users.noreply.github.com>
2024-06-26 19:11:21 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@
 
 import os
 import sys
-sys.path.insert(0,os.path.abspath('../src/epspy/'))
 
 #-----------------------------------------------------------------------------
 
@@ -22,6 +21,8 @@ def setup(app):
 	app.connect('builder-inited', run_apidoc)
 
 # ----------------------------------------------------------------------------
+
+sys.path.insert(0,os.path.abspath('../src/epspy/'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
