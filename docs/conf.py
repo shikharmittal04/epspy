41cec21ffabc330f37872b13e9e358bccc1aee42
Shikhar Mittal <60473059+shikharmittal04@users.noreply.github.com>
2024-06-26 12:37:10 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0,os.path.abspath('../src/epspy/'))
 #Adding this to generate API reference on readthedocs website
 def run_apidoc(_):
 	from sphinx.apidoc import main
-  modules = 'meps'
+	modules = 'meps'
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 	cur_dir = os.path.abspath(os.path.dirname(__file__))
 	main(['-e', '-o', cur_dir, module, '--force'])
