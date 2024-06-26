7c897c317eac008858beb45899468088a16a523a
Shikhar Mittal <60473059+shikharmittal04@users.noreply.github.com>
2024-06-26 19:07:33 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ def run_apidoc(_):
 	from sphinx.ext.apidoc import main
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 	cur_dir = os.path.abspath(os.path.dirname(__file__))
- 	module = os.path.join(cur_dir,"..","meps")
+	module = os.path.join(cur_dir,"..","meps")
 	main(['-e', '-o', cur_dir, module, '--force'])
 
 def setup(app):
